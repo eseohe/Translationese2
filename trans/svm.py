0afddf26cd14f79cd65f2c7a28df62148fbd95d8
Eustace Ebhotemhen <eebhotemhen@gmail.com>
2016-08-24 19:32:40 +0200

    first commit

---
+++ trans/svm.py
@@ -0,0 +1,97 @@
+'''
+Created on Aug 23, 2016
+
+@author: admin
+'''
+from trans.classifier import classifier
+import scipy
+import numpy as np
+import sklearn
+from sklearn.svm import SVC
+from sklearn import cross_validation
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
+import os
+import pickle
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.ensemble import RandomForestClassifier
+#from scipy.stats import randint as sp_randint
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score, recall_score
+
+
+class svm(classifier):
+    '''
+    classdocs
+    '''
+
+
+    C = 1.0
+    k = 'linear'  #linear kernel
+    max_iter = -1
+    gamma = 'auto'
+    
+    
+    
+    def __init__(self, X, y):
+        classifier.__init__(self, X, y)
+        
+    
+
+    def splitTrainTest(self):
+        self.Xtrain, self.Xtest, self.ytrain, self.ytest = cross_validation.train_test_split(self.X, self.y, 
+                                                                                            test_size=self.splitPercent,
+                                                                                            random_state=0)
+                                                                                            
+    def train(self):
+        
+        if self.n_foldCV <= 0:
+            print ('No cross validation required. If required set the parameter to a positive number')
+            clf = SVC(kernel  = self.k)
+            clf.fit(self.Xtrain, self.ytrain)
+        else:
+            score = 'recall'
+            # Set the parameters by cross-validation
+            tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+                    
+                    
+            clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=self.n_foldCV,
+                       scoring='%s_weighted' % score)
+            
+            clf.fit(self.Xtrain, self.ytrain)
+            
+        self.model = clf
+            
+            
+    def saveModel(self, location, saveAs):
+        if os.path.exists(location):
+            with open(location+'/'+saveAs, 'wb') as output:
+                pickle.dump(self.model, output, pickle.HIGHEST_PROTOCOL)
+        else:
+            print ('Please enter a valid directory')
+            
+    def loadModel(self, location, savedAs):
+        if os.path.exists(location):
+            if os.path.isfile(location+'/'+savedAs):
+                with open(location+'/'+savedAs, 'rb') as input:
+                    self.model = pickle.load(input)
+            else:
+                print ('file does not exist')
+        else:
+            print ('Please enter a valid directory')
+    
+
+    def predict(self):
+        return self.model.predict(self.Xtest)
+        
+        
+        
+        
+    def evaluate(self):
+        y_pred = self.predict()
+        print ('Accuracy: ', accuracy_score(self.ytest, y_pred))
+        print ('Precision: ', average_precision_score(self.ytest, y_pred))
+        print ('Recall: ', recall_score(self.ytest, y_pred))
+        print ('F-score: ', f1_score(self.ytest, y_pred))
